c67e67739a5858dd8ff36021f40e3883295fa4ec
592363789 <592363789@qq.com>
2020-11-18 20:41:24 +0800

    add in attributes in Email.java

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,22 @@
 package emailapp;
 
 public class Email {
+    private String firstName;
+    private String lastName;
+    private String psw;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
 
+    //Constructor to receive the first name and last name
+
+    // Ask for the department
+
+    //Gernerate a random password
+
+    //Set the mailbox capacity
+
+    // Set the alternate email
+
+    // Change the password
 }
