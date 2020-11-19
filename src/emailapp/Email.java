45368ebe9627159b1abc50d4878440ae6fc46268
592363789 <592363789@qq.com>
2020-11-19 16:59:47 +0800

    add in setdepartment() and add in user names

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
     private String lastName;
@@ -9,9 +11,30 @@ public class Email {
     private String alternateEmail;
 
     //Constructor to receive the first name and last name
+    public Email (String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("Email: " + this.firstName + " " + this.lastName);
 
+        // Call a method asking for the department - return the department
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
+    }
     // Ask for the department
-
+    private String setDepartment() {
+        System.out.print("DEPARTMENT CODES\n1 for Sales\n2 for Deveopment\n3 for Accounting\n0 for none\nEnter the department code: ");
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+        if (depChoice == 1) {
+            return "sales";
+        } else if (depChoice == 2) {
+            return "dev";
+        } else if (depChoice == 3) {
+            return "acct";
+        } else {
+            return "";
+        }
+    }
     //Gernerate a random password
 
     //Set the mailbox capacity
--- src/emailapp/emailApp.java
+++
@@ -1,7 +0,0 @@
-package emailapp;
-
-public class emailApp {
-    public static void main((String[] args)) {
-
-    }
-}
