d7e28ceb21a3ba69496eb8800d3abbd832577afd
592363789 <592363789@qq.com>
2020-11-19 19:23:02 +0800

    add in randomPassword()

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -7,6 +7,7 @@ public class Email {
     private String lastName;
     private String psw;
     private String department;
+    private int defaultPasswordLength = 5;
     private int mailboxCapacity;
     private String alternateEmail;
 
@@ -19,6 +20,10 @@ public class Email {
         // Call a method asking for the department - return the department
         this.department = setDepartment();
         System.out.println("Department: " + this.department);
+
+        //Call a method that returns a random password
+        this.psw = randomPassword(defaultPasswordLength);
+        System.out.println("Your password: " + this.psw);
     }
     // Ask for the department
     private String setDepartment() {
@@ -35,8 +40,17 @@ public class Email {
             return "";
         }
     }
-    //Gernerate a random password
 
+    //Gernerate a random password
+    private String randomPassword(int len) {
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
+        char[] password = new char[len];
+        for (int i = 0; i < len; i++) {
+            int rand = (int)(Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(rand);
+        }
+        return new String(password);
+    }
     //Set the mailbox capacity
 
     // Set the alternate email
